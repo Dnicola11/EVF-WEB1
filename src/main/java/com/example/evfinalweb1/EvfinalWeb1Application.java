7cc5cd3b9ccf382825d641bd329d6f9e18ac4ebb
David Nicola <davidnicola1098@hotmail.com>
2023-08-24 12:07:31 -0500

    OK

---
+++ src/main/java/com/example/evfinalweb1/EvfinalWeb1Application.java
@@ -0,0 +1,13 @@
+package com.example.evfinalweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvfinalWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvfinalWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evfinalweb1/EvfinalWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evfinalweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvfinalWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
